985f9555cbb1173054e2013c1c4d1542273108d2
Pranav Ajith <pranav.ajith.cse21@itbhu.ac.in>
2024-09-17 16:06:55 +0530

    Testing Hello name

--- main_test.go
+++ main_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello Word"
+	got := Hello("Prana")
+	want := "Hello Pranav!"
 
 	if got != want {
 		t.Errorf("got %q wanted %q", got, want)
